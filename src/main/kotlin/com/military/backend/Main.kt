3e12a8e3f9565f31a2f65d23c2f3ee8172ea3fa8
Mikhail Likholetov <odaving111@gmail.com>
2022-07-14 17:56:30 +0300

    Spring Boot Application added

--- src/main/kotlin/com/military/backend/Main.kt
+++ src/main/kotlin/com/military/backend/Main.kt
@@ -1,5 +1,13 @@
 package com.military.backend
 
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@SpringBootApplication
+@EnableScheduling
+class Application
+
 fun main() {
-    println("Hello world")
+    SpringApplication.run(Application::class.java)
 }
