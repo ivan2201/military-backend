9e97d835c679ac451662a3bfbbebfaa0105f8800
Mikhail Likholetov <odaving111@gmail.com>
2022-07-25 09:26:12 +0300

    DTO for json added

---
+++ src/main/kotlin/com/military/backend/domain/dto/ObjectInformatizationDTO.kt
@@ -0,0 +1,11 @@
+package com.military.backend.domain.dto
+
+data class ObjectInformatizationDTO(
+    val id: Int,
+    val name: String?,
+    val dateUpdate: String?,
+    val numberOfDocuments: String?,
+    val cert: String?,
+    val si: String?,
+    val scr: String?
+)
---
+++ src/main/kotlin/com/military/backend/domain/dto/WarCampDTO.kt
@@ -0,0 +1,8 @@
+package com.military.backend.domain.dto
+
+data class WarCampDTO(
+    val id: Int,
+    val name: String?,
+    val number: String?,
+    val location: String?
+)
