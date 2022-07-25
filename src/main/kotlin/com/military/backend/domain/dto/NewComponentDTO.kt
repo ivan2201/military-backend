055c68a70a6a0db34d8999992b4b1005a1a178d9
Mikhail Likholetov <odaving111@gmail.com>
2022-07-25 10:00:21 +0300

    DTO for post request added

---
+++ src/main/kotlin/com/military/backend/domain/dto/NewComponentDTO.kt
@@ -0,0 +1,7 @@
+package com.military.backend.domain.dto
+
+data class NewComponentDTO(
+    val name: String?,
+    val series: String?,
+    val oiId: Int?
+)
---
+++ src/main/kotlin/com/military/backend/domain/dto/NewWarCampDTO.kt
@@ -0,0 +1,7 @@
+package com.military.backend.domain.dto
+
+data class NewWarCampDTO(
+    val name: String?,
+    val number: String?,
+    val location: String?
+)
